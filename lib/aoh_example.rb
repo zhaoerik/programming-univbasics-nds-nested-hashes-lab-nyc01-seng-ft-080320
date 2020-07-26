7cb99ebc1535451117971e453b8ade348ef5bdef
Erik Zhao <erikzhao98@gmail.com>
2020-07-26 18:33:11 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -18,7 +18,7 @@ def literal_aoh
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
   array = [
-    {:name => "Done Gately", :occupation => "Live-in Staff"},
+    {:name => "Don Gately", :occupation => "Live-in Staff"},
     {:name => "Joelle van Dyne", :occupation => "Radio Personality"},
     {:name => "Pat Monteseian", :occupation => "Staff"},
     {:name => "Kate Gompert", :occupation => "None"},
